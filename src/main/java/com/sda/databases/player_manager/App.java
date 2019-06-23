41a8d86e6c3b2ca11495022a266dc62a65810010
Stefania Mois <stefaniamois@gmail.com>
2019-06-23 10:47:21 +0300

    first commit

---
+++ src/main/java/com/sda/databases/player_manager/App.java
@@ -0,0 +1,13 @@
+package com.sda.databases.player_manager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
